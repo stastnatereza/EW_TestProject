dd6e0ac572aa47c932de7b71466cb442e84b37ec
stastnatereza <stastna.tery@gmail.com>
2026-09-21 01:30:39 +0200

    Add files via upload

--- notebooks/Bronze_Ingest_Payments.py
+++ notebooks/Bronze_Ingest_Payments.py
@@ -1,1 +1,12 @@
+file_path = "Files/DS3_Payments.csv" 
+table_name = "dbo.Payments"
 
+df = (
+    spark.read.format("csv")
+    .option("header", "true")       # První řádek jako název sloupce
+    .option("delimiter", ";")       # Definice oddělovače
+    .option("inferSchema", "false") # Všechny datové typy jako string beze změny
+    .load(file_path)
+)
+
+df.write.format("delta").mode("overwrite").saveAsTable(table_name)
